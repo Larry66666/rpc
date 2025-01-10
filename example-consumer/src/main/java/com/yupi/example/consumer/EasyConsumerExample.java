46accba5f6cb3ada9e9e3f35b0d0163a5187f99d
Larry <1668527392@qq.com>
2025-01-10 17:51:36 +0800

    简易版rpc框架

--- example-consumer/src/main/java/com/yupi/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/yupi/example/consumer/EasyConsumerExample.java
@@ -1,2 +1,27 @@
-package com.yupi.example.consumer;public class EasyConsumerExample {
+package com.yupi.example.consumer;
+
+import com.yupi.example.common.model.User;
+import com.yupi.example.common.service.UserService;
+import com.yupi.yurpc.proxy.ServiceProxyFactory;
+
+/**
+ * 简易消费者示例
+ */
+public class EasyConsumerExample {
+
+    public static void main(String[] args) {
+        // 静态代理
+        //UserService userService = new UserServiceProxy();
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("yupi");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(user.getName());
+        } else {
+            System.out.println("user is null");
+        }
+    }
 }
---
+++ yu-rpc-easy/src/main/java/com/yupi/yurpc/server/VertxHttpServer.java
@@ -0,0 +1,31 @@
+package com.yupi.yurpc.server;
+
+import io.vertx.core.Vertx;
+
+public class VertxHttpServer implements HttpServer {
+
+    /**
+     * 启动服务器
+     * @param port
+     */
+    @Override
+    public void doStart(int port) {
+        // 创建Vert.x 实例
+        Vertx vertx = Vertx.vertx();
+
+        // 创建HTTP服务器
+        io.vertx.core.http.HttpServer server = vertx.createHttpServer();
+
+        // 监听端口并处理请求
+        server.requestHandler(new HttpServerHandler());
+
+        // 启动HTTP服务器并监听指定端口
+        server.listen(port, result -> {
+           if (result.succeeded()) {
+               System.out.println("Server is now listening on port " + port);
+           } else {
+               System.err.println("Failed to start server: " + result.cause());
+           }
+        });
+    }
+}
